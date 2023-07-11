6f1e039d94906419cd2b4121fff8b076c0cd8714
HYUNJEE <hyunjeesong@gmail.com>
2023-07-11 19:30:49 +0900

    생활코딩

--- react_study/react-app/src/App.js
+++ react_study/react-app/src/App.js
@@ -1,53 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props){
+function Header(props) {
   console.log('props', props, props.title)
   return <header>
-    <h1><a href="/" onClick={(event)=>{
+    <h1><a href="/" onClick={(event) => {
       event.preventDefault();
       props.onChangeMode();
     }}>{props.title}</a></h1>
   </header>
 }
-function Nav(props){
-  const lis=[]
-  for(let i=0; i<props.topics.length; i++){
+function Nav(props) {
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
     lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{event.preventDefault();
-      props.onChangeMode(event.target.id);}}>{t.title}</a>
-      </li>)
+      <a id={t.id} href={'/read/' + t.id} onClick={event => {
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>)
   }
   return <nav>
-  <ol>
-    {lis}
-  </ol>
-</nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
 }
-function Article(props){
+function Article(props) {
   return <article><h2>{props.title}</h2>{props.body}</article>
 }
 
 function App() {
-  const mode='READ';
-  const topics =[
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javas', body:'javas is ...'}
+  const mode = 'READ';
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javas', body: 'javas is ...' }
   ]
   let conent = null;
-  if (mode==='WELCOME'){
-    content = <Article title = 'welcome' body='HELLO, WEB'></Article>
-  }else if (mode==='READ'){
-    content=<Article title ='Read' body='hello, read'></Article>
+  if (mode === 'WELCOME') {
+    content = <Article title='welcome' body='HELLO!, WEB'></Article>
+  } else if (mode === 'READ') {
+    content = <Article title='Read' body='hello, read'></Article>
   }
 
   return (
     <div className="App">
-      <Header title="REACT" onChangeMode={()=>{
-        mode='WELCOME';
+      <Header title="REACT" onChangeMode={() => {
+        mode = 'WELCOME';
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{mode='READ';}} ></Nav>
+      <Nav topics={topics} onChangeMode={(id) => { mode = 'READ'; }} ></Nav>
       <Article title='WELCOME' body='hello, weBB'></Article>
     </div>
   );
