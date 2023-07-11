4ba6eeb8f24af2ce7314cb083981b2b0744963b6
HYUNJEE <hyunjeesong@gmail.com>
2023-07-11 19:14:53 +0900

    생활코딩 - REACT

--- react_study/react-app/src/App.js
+++ react_study/react-app/src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 function Header(props){
   console.log('props', props, props.title)
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 function Nav(props){
   const lis=[]
   for(let i=0; i<props.topics.length; i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{event.preventDefault();
+      props.onChangeMode(event.target.id);}}>{t.title}</a>
+      </li>)
   }
   return <nav>
   <ol>
@@ -23,15 +29,25 @@ function Article(props){
 }
 
 function App() {
+  const mode='READ';
   const topics =[
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javas', body:'javas is ...'}
   ]
+  let conent = null;
+  if (mode==='WELCOME'){
+    content = <Article title = 'welcome' body='HELLO, WEB'></Article>
+  }else if (mode==='READ'){
+    content=<Article title ='Read' body='hello, read'></Article>
+  }
+
   return (
     <div className="App">
-      <Header title="REACT"></Header>
-      <Nav></Nav>
+      <Header title="REACT" onChangeMode={()=>{
+        mode='WELCOME';
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{mode='READ';}} ></Nav>
       <Article title='WELCOME' body='hello, weBB'></Article>
     </div>
   );
