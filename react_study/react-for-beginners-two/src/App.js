c59a3f778d14e63ab2d3cee2f821d0e582e369e7
kelly062001 <hyunjeesong95@gmail.com>
2023-08-11 01:13:38 +0900

    5 완료

---
+++ react_study/react-for-beginners-two/src/App.js
@@ -0,0 +1,18 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import {useState} from "react";
+
+function App() {
+  const [counter, setValue]=useState(0);
+  const onClick =()=> setValue((prev)=>prev+1);
+  console.log("call an api");
+  return (
+    <div>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <Button text={"continue"}/>
+    </div>
+  );
+}
+
+export default App;
