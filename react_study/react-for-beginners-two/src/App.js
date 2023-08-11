350cd0ddc3c6f4730fd61ae808135f94ee442cd2
kelly062001 <hyunjeesong95@gmail.com>
2023-08-11 16:26:05 +0900

    coin tracker

--- react_study/react-for-beginners-two/src/App.js
+++ react_study/react-for-beginners-two/src/App.js
@@ -1,18 +1,25 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
-  const [counter, setValue]=useState(0);
-  const onClick =()=> setValue((prev)=>prev+1);
-  console.log("call an api");
-  return (
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] =useState([])
+  const [money, setMoney] =useState(0);
+  const onChange = (event) => setMoney(event.target.value); 
+  console.log(money);
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers").then((response)=>response.json()).then((json)=>{setCoins(json); 
+    setLoading(false);});
+  }, [])
+  return(
     <div>
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      <Button text={"continue"}/>
+      <h1>the coins!{loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>loading...</strong> : <select>
+        {coins.map((item)=><option>{item.name} ({item.symbol}) : {item.quotes.USD.price}</option>)}
+      </select>}
     </div>
-  );
+  )
 }
 
 export default App;
