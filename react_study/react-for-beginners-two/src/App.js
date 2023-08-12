257434e23d3fcbe6800629b6f6621248a1834399
kelly062001 <hyunjeesong95@gmail.com>
2023-08-12 13:57:09 +0900

    browserrouter -> hashrouter

--- react_study/react-for-beginners-two/src/App.js
+++ react_study/react-for-beginners-two/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
