d9c43cc6c043beba0eaee973c8ebf32a9cb4f4d4
kelly062001 <hyunjeesong95@gmail.com>
2023-08-12 13:52:18 +0900

    publish

--- react_study/react-for-beginners-two/src/App.js
+++ react_study/react-for-beginners-two/src/App.js
@@ -5,9 +5,9 @@ import Detail from "./routes/Detail";
 
 function App() {
 return (
-<BrowserRouter>
+<BrowserRouter basename={process.env.PUBLIC_URL}>
   <Routes>
-    <Route path="/" element={<Home/>} />
+    <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>} />
     <Route path="/movie/:id" element={<Detail />} />
   </Routes>
 </BrowserRouter>
